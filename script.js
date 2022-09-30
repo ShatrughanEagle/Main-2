c98ee8463363cd68b6ad16a996cfea9b26c6783d
ShatrughanEagle <97624814+ShatrughanEagle@users.noreply.github.com>
2022-09-30 09:27:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const splash = document.querySelector('.splash');
+document.addEventListener('DOMContentLoaded',()=>{
+    setTimeout(()=>{
+        splash.classList.add('active');
+    }, 2000)
+})
